ab615fcde0652e4098f514e6e639e73465f97a85
Evan Strat <evan10s@users.noreply.github.com>
2016-11-12 13:52:18 -0500

    Add questions page and MDL

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: '20 Questions'
+    });
 });
-
+router.get('/:id', function (req, res, next) {
+    res.render('index', {
+        title: '20 Questions'
+        , id: req.params.id
+    });
+});
 module.exports = router;
