8b11caa65f6073f9c0742aecc93a6b7cf5874bf8
Ayush Goyal <agoyal2001@gmail.com>
2016-11-13 10:06:27 -0500

    Minor view changes

--- public/javascript/questions.js
+++ public/javascript/questions.js
@@ -85,8 +85,8 @@ function sendQuestionResponse(val) {
 		questionsToAnswer.splice(0, 1);
 		console.log("new questionsToAnswer", questionsToAnswer);
 		if (gameFinished) {
-			writeChatData("Congrats, you got it!", "System");
-			writeChatData("<a href='../../'> Create a new game</a>", "System");
+			writeChatData("Congrats, you guessed it!", "System");
+			writeChatData(" <a href='../../'> Create a new game</a>", "System");
 		} else {
 			showNextQuestion();
 		}
