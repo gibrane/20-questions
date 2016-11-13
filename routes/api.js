130bd33144c6499a4586c341c5a9230bdcff55dd
gib98 <gibran.essa@gmail.com>
2016-11-12 22:55:58 -0500

    random word generator

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,10 @@
 var express = require('express');
-var request = require('request');
 var router = express.Router();
 /* GET home page. */
-var words = [];
-
-function getWord(res) {
-    request("http://www.setgetgo.com/randomword/get.php?len=6", function (error, response, body) {
-        words.push(body);
-        if (words.length === 1) {
-            res.send(words);
-            words = [];
-        }
-        else {
-            setTimeout(function () {
-                getWord(res);
-            }, 60);
-        }
-    })
-}
+var adjectives = ["able", "bad", "best", "better", "big", "black", "certain", "clear", "different", "early", "easy", "economic", "federal", "free", "full", "good", "great", "hard", "high", "human", "important", "international", "large", "late", "little", "local", "long", "low", "major", "military", "national", "new", "old", "only", "other", "political", "possible", "public", "real", "recent", "right", "small", "social", "special", "strong", "sure", "true", "white", "whole", "young"];
+var nouns = ["time", "year", "people", "way", "day", "man", "thing", "woman", "life", "child", "world", "school", "state", "family", "student", "group", "country", "problem", "hand", "part", "place", "case", "week", "company", "system", "program", "question", "work", "government", "number", "night", "point", "home", "water", "room", "mother", "area", "money", "story", "fact", "month", "lot", "right", "study", "book", "eye", "job", "word", "business", "issue", "side", "kind", "head", "house", "service", "friend", "father", "power", "hour", "game", "line", "end", "member", "law", "car", "city", "community", "name", "president", "team", "minute", "idea", "kid", "body", "information", "back", "parent", "face", "others", "level", "office", "door", "health", "person", "art", "war", "history", "party", "result", "change", "morning", "reason", "research", "girl", "guy", "moment", "air", "teacher", "force", "education"];
 router.get('/roomname', function (req, res, next) {
-    getWord(res);
+    var roomCode = adjectives[Math.floor(Math.random() * adjectives.length)] + "-" + nouns[Math.floor(Math.random() * nouns.length)];
+    res.send(roomCode);
 });
 module.exports = router;
