687e4395f817b2cf97440db06d55c49599b1e275
Ayush Goyal <agoyal2001@gmail.com>
2016-11-13 00:38:58 -0500

    Fixed questions and chat scrolling

--- public/javascript/questions.js
+++ public/javascript/questions.js
@@ -1,8 +1,30 @@
+var currentLoginUserObj;
+var currentLoginUsername;
+firebase.auth().onAuthStateChanged(function (user) {
+	if (user) {
+		currentLoginUserObj = user;
+		getCurrentUsername();
+	}
+});
+
+function getCurrentUsername() {
+	firebase.database().ref('usernames/' + currentLoginUserObj.uid).once('value').then(function (snapshot) {
+		currentLoginUsername = snapshot.val().username;
+	});
+}
+
+//Get window height for scrolling of chat box
+var windowHeight = $(window).height();
+$('#chat-ul').css("max-height", windowHeight - 175);
+$('#questions-tab').css("max-height", windowHeight - 80);
+
 $('#question-input').keydown(function (event) {
 	if (event.keyCode == 13) {
-		$("#questions-div").prepend(
+		$("#questions-ul").append(
 			'<li class="mdl-list__item">' + $("#question-input").val() + '<span class="mdl-list__item-primary-content"></span> <i class="material-icons mdl-list__item-icon answer">help</i></li>');
 		$("#question-input").val('');
+		var div = $('#questions-tab');
+		div.scrollTop(div.prop("scrollHeight"));
 	}
 });
 
@@ -29,5 +51,14 @@ firebase.database().ref('rooms/' + roomName + "/members").once('value').then(fun
 }).then(function () {
 	for (var player in playersList) {
 		addPlayer(player);
+	}
+});
+
+$('#chat-input').keydown(function (event) {
+	if (event.keyCode == 13) {
+		$("#chat-ul").append('<li class="mdl-list__item"> <span class="mdl-list__item-primary-content">' + currentLoginUsername + ": " + $("#chat-input").val() + '</span> </li>')
+		$("#chat-input").val('');
+		var div = $('#chat-ul');
+		div.scrollTop(div.prop("scrollHeight"));
 	}
 });
